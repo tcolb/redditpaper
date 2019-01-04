3ae4c28960f51cd3eb3f47889681fcdc6d2656b9
tcolb <tristan.colb@gmail.com>
2019-01-04 10:50:35 -0800

    wallpaper manager and url bitmap decoding working

--- app/src/main/java/com/trco/redditpaper/MainActivity.kt
+++ app/src/main/java/com/trco/redditpaper/MainActivity.kt
@@ -3,12 +3,16 @@ package com.trco.redditpaper
 import android.app.DownloadManager
 import android.app.WallpaperManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.net.Uri
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat.getSystemService
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import org.json.JSONObject
 import java.io.BufferedInputStream
@@ -19,36 +23,43 @@ import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
-    val dlManager: DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-    val wpManager: WallpaperManager = getSystemService(Context.WALLPAPER_SERVICE) as WallpaperManager
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val dlManager: DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val wpManager: WallpaperManager = getSystemService(Context.WALLPAPER_SERVICE) as WallpaperManager
+
         val grabTask = FetchSubredditTask().execute(URL("https://www.reddit.com/r/analog.json"))
         val srJSON = JSONObject(grabTask.get())
         var parseTask = ParseJSONTask().execute(srJSON)
 
+        val bmpTask = UrlBitmapTask().execute(URL("https://i.redd.it/l8mareg8l8821.jpg"))
+
+
+        /*
         var dlReq = DownloadManager.Request(Uri.parse("https://i.redd.it/l8mareg8l8821.jpg"))
         dlReq.setTitle("Example2!")
         dlReq.setDescription("Downloading")
         dlReq.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         dlReq.setVisibleInDownloadsUi(true)
-        //dlReq.setDestinationUri("")
+        dlReq.setDestinationUri("")
         dlManager.enqueue(dlReq)
 
-        /*
+
         for (url in parseTask.get()) {
             var uri = Uri.parse(url.toURI().toString())
             dlManager.enqueue(DownloadManager.Request(uri))
         }
         */
 
-        val tv = findViewById<TextView>(R.id.mainText)
+        val tv = findViewById<TextView>(R.id.testText)
         tv.text = srJSON.toString()
+        val iv = findViewById<ImageView>(R.id.testImage)
+        iv.setImageBitmap(bmpTask.get())
 
+        wpManager.setBitmap(bmpTask.get())
     }
 
     private class FetchSubredditTask : AsyncTask<URL, Int, String>() {
@@ -97,16 +108,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private class SetWallpaperTask: AsyncTask<URL, Int, Int>() {
-        override fun onPostExecute(result: Int?) {
+    private class UrlBitmapTask: AsyncTask<URL, Int, Bitmap>() {
+        val TAG = "UrlBitmapTask"
+
+        override fun onPostExecute(result: Bitmap?) {
+            Log.v(TAG, "finished decoding bitmap from url")
             super.onPostExecute(result)
         }
 
-        override fun doInBackground(vararg params: URL?): Int {
+        override fun doInBackground(vararg params: URL?): Bitmap {
+            var bitmap: Bitmap
             val urlConnection: HttpsURLConnection = params[0]?.openConnection() as HttpsURLConnection
             try {
                 val inStream: InputStream = BufferedInputStream(urlConnection.inputStream)
+                bitmap = BitmapFactory.decodeStream(inStream)
+            } finally {
+                urlConnection.disconnect()
             }
+            return bitmap
         }
     }
 }
